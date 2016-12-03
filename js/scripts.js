9657cd61042ad42cf431c92840d7fc63b72cfcba
Dolan Miu <dolan_miu@hotmail.com>
2016-12-03 02:04:23 +0000

    modernised the entire website

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function ($) {
+jQuery(document).ready(function($) {
 
 
     $(window).stellar();
@@ -13,20 +13,19 @@ jQuery(document).ready(function ($) {
     mywindow = $(window);
     htmlbody = $('html,body');
 
-    slide.waypoint(function (event, direction) {
+    slide.waypoint(function(event, direction) {
 
         dataslide = $(this).attr('data-slide');
 
         if (direction === 'down') {
             $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
-        }
-        else {
+        } else {
             $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
         }
 
     });
- 
-    mywindow.scroll(function () {
+
+    mywindow.scroll(function() {
         if (mywindow.scrollTop() == 0) {
             $('.navigation li[data-slide="1"]').addClass('active');
             $('.navigation li[data-slide="2"]').removeClass('active');
@@ -36,15 +35,15 @@ jQuery(document).ready(function ($) {
     function goToByScroll(dataslide) {
         htmlbody.animate({
             scrollTop: $('.slide[data-slide="' + dataslide + '"]').offset().top
-        }, 2000, 'easeInOutQuint');
+        }, 500, 'easeInOutQuint');
     }
 
     function getPictureURLs() {
         $.ajax({
             url: "/photos",
-            success: function(data){
-                $(data).find("a:contains(.JPG)").each(function(){
-                // will loop through 
+            success: function(data) {
+                $(data).find("a:contains(.JPG)").each(function() {
+                    // will loop through 
                     //alert("Found a file: " + $(this).attr("href"));
                     $("#gallery").append('<div id="picture" class="grid_3 omega"><a class="image-link" href="photos/' + $(this).attr("href") + '" data-lightbox="image-1"><div class="image"><img src="photos/' + $(this).attr("href") + '" /></div></a></div>');
                 });
@@ -54,24 +53,24 @@ jQuery(document).ready(function ($) {
 
     function initialize() {
         var mapProp = {
-            center:new google.maps.LatLng(51.508742,-0.120850),
-            zoom:15,
-            mapTypeId:google.maps.MapTypeId.ROADMAP
-            };
-        var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+            center: new google.maps.LatLng(51.508742, -0.120850),
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
     }
 
     function initJarallax() {
         var jarallax = new Jarallax(new ControllerScroll(true));
     }
 
-    links.click(function (e) {
+    links.click(function(e) {
         e.preventDefault();
         dataslide = $(this).attr('data-slide');
         goToByScroll(dataslide);
     });
 
-    button.click(function (e) {
+    button.click(function(e) {
         e.preventDefault();
         dataslide = $(this).attr('data-slide');
         goToByScroll(dataslide);
--- php/reviewScraper.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('simple_html_dom.php');  
-
-$articles = array();  
-getArticles('http://net.tutsplus.com/page/76/');  
-
-?>
