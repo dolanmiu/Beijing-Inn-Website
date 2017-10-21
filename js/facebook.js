df84d4db16817d06a493d8fba6a338d98af8ffe9
Dolan <dolan_miu@hotmail.com>
2017-10-21 19:36:40 +0100

    Fix API change with access_token

--- js/facebook.js
+++ js/facebook.js
@@ -1,3 +1,4 @@
+// this produces an object: {access_token: string, token_type: string}
 function getAuthToken(callback) {
     $.ajax({
         url: `https://graph.facebook.com/oauth/access_token?client_id=223677548043608&client_secret=14de0c5e090951821c8ab9eaaf50f7c7&grant_type=client_credentials`,
@@ -10,7 +11,7 @@ function getAuthToken(callback) {
 function getPhotos(callback) {
     getAuthToken(function(authToken) {
         $.ajax({
-            url: `https://graph.facebook.com/v2.7/beijinginn/photos?${authToken}&type=uploaded&limit=24`,
+            url: `https://graph.facebook.com/v2.7/beijinginn/photos?${authToken.access_token}&type=uploaded&limit=24`,
             success: function(photos) {
                 callback(photos.data);
             }
